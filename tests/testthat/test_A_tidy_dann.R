da91adf4afbe4411f7fe25c2e1bbbe67ca5caab4
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2023-09-09 14:35:31 -0500

    test tidy_dann

---
+++ tests/testthat/test_A_tidy_dann.R
@@ -0,0 +1,71 @@
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(tidymodels))
+suppressPackageStartupMessages(library(mlbench))
+library(dann)
+
+###############################################
+# Formula and recipe interface work
+###############################################
+# dann does not do much during fit.
+# So data does not matter.
+# Confirming all interfaces work and return the
+# same results.
+
+set.seed(1)
+train <- mlbench.2dnormals(1000, cl = 2, r = sqrt(2), sd = .2) %>%
+  tibble::as_tibble()
+colnames(train) <- c("X1", "X2", "Y")
+
+
+model <- tidy_dann() %>%
+  set_engine("dann")
+
+rec_obj <- recipe(Y ~ X1 + X2, data = train)
+wf <- workflow() %>%
+  add_model(model) %>%
+  add_recipe(rec_obj)
+
+
+test_that("No errors?", {
+  expect_no_error(tidy_dann() %>%
+    set_engine("dann") %>%
+    fit(formula = Y ~ ., data = train))
+  expect_no_error(wf %>%
+    fit(data = train))
+})
+
+model_01 <- tidy_dann() %>%
+  set_engine("dann") %>%
+  fit(formula = Y ~ ., data = train)
+pred_01 <- model_01 %>%
+  predict(new_data = train)
+
+model_02 <- dann(formula = Y ~ ., data = train)
+pred_02 <- model_02 %>%
+  predict(new_data = train)
+
+model_03 <- wf %>%
+  fit(data = train)
+pred_03 <- model_03 %>%
+  predict(new_data = train)
+
+model_04 <- dann(rec_obj, data = train)
+pred_04 <- model_04 %>%
+  predict(new_data = train)
+
+test_that("Results match?", {
+  expect_equal(pred_01, pred_02)
+  expect_equal(pred_03, pred_04)
+})
+
+rm(train, rec_obj, model, wf)
+rm(model_01, model_02, model_03, model_04)
+rm(pred_01, pred_02, pred_03, pred_04)
+
+###############################################
+# default values match
+###############################################
+test_that("Defalut values match?", {
+  expect_true(formals(dann)$k == formals(tidy_dann)$neighbors)
+  expect_true(formals(dann)$epsilon == formals(tidy_dann)$epsilon)
+})
