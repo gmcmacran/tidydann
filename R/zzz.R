221b7a7b980fd1c6de5fce15c1b4c670d2dc6edb
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2023-09-12 08:11:05 -0500

    better style

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,10 @@
   make_tidy_dann_model()
   make_tidy_sub_dann_model()
 
-  vctrs::s3_register("generics::tunable", "tidy_dann", tunable_tidy_dann)
+  vctrs::s3_register(
+    "generics::tunable", "tidy_dann",
+    tunable_tidy_dann
+  )
   vctrs::s3_register(
     "generics::tunable", "tidy_sub_dann",
     tunable_tidy_sub_dann
