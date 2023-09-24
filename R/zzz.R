54bdc663a3136055a8cc91e7ea836bdd6eddf5b6
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2023-09-24 09:23:20 -0500

    coverage ignore

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,15 @@
+# nocov start
+
+# ------------------------------------------------------------------------------
+
+# The functions below define the model information. These access the model
+# environment inside of parsnip so they have to be executed once parsnip has
+# been loaded.
+
 #' @keywords internal
 .onLoad <- function(libname, pkgname) {
   make_tidy_dann_dann()
   make_tidy_sub_dann_sub_dann()
 }
+
+# nocov end
